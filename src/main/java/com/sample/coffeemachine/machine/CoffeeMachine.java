3b1dc001b45a763f732f134669029d06f2fc99ef
om-nitp <56344889+om-nitp@users.noreply.github.com>
2021-06-21 01:38:54 +0530

    Updated coffee machine

--- src/main/java/com/sample/coffeemachine/machine/CoffeeMachine.java
+++ src/main/java/com/sample/coffeemachine/machine/CoffeeMachine.java
@@ -15,36 +15,42 @@ import com.sample.coffeemachine.model.AvailableResources;
 import com.sample.coffeemachine.model.Outlet;
 
 public class CoffeeMachine {
-    private final List<Outlet> outlets;
+    private final Map<Integer, Outlet> outlets;
     private final AvailableResources availableResources;
     private final Brewer brewer;
 
     public CoffeeMachine(int totalOutlets) {
         this.availableResources = new AvailableResources(InitialResources.initialResources);
-        this.outlets = new ArrayList<>();
+        this.outlets = new HashMap<>();
         for(int i=1; i<=totalOutlets; i++) {
-            outlets.add(new Outlet(i, "Outlet number: " + i));
+            outlets.put(i, new Outlet(i, "Outlet number: " + i));
         }
         this.brewer = new Brewer(availableResources);
     }
 
     public void brewBeverage(int outletId, BeverageType beverageType) throws InterruptedException {
-        Outlet outlet = outlets.get(outletId-1);
+        if(!outlets.containsKey(outletId)) {
+            System.out.println(outletId + " is not present");
+            return;
+        }
+        Outlet outlet = outlets.get(outletId);
         synchronized (this) {
             if (outlet.getOutletStatus() != IDLE) {
                 System.out.println("Outlet number " + outletId + " is not available");
                 return;
             }
+            outlet.updateStatus(IN_PROGRESS);
         }
-        outlet.updateStatus(IN_PROGRESS);
 
         brewer.setOutlet(outlet);
         brewer.setBeverageType(beverageType);
-        Thread thread = new Thread(brewer);
+        Thread newBrewer = new Thread(brewer);
         try {
-            thread.start();
+            newBrewer.start();
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
+        } finally {
+            outlet.updateStatus(IDLE);
         }
     }
 
