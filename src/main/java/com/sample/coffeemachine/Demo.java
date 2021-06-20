604f49e56164492d703831805fbf5cf78f98706f
om-nitp <56344889+om-nitp@users.noreply.github.com>
2021-06-21 02:14:28 +0530

    updated demo

--- src/main/java/com/sample/coffeemachine/Demo.java
+++ src/main/java/com/sample/coffeemachine/Demo.java
@@ -1,6 +1,10 @@
 package com.sample.coffeemachine;
 
 import static com.sample.coffeemachine.beverages.BeverageType.HOT_WATER;
+import static com.sample.coffeemachine.beverages.BeverageType.HOT_TEA;
+import static com.sample.coffeemachine.beverages.BeverageType.COFFEE;
+import static com.sample.coffeemachine.beverages.BeverageType.BLACK_TEA;
+import static com.sample.coffeemachine.beverages.BeverageType.GREEN_TEA;
 
 import com.sample.coffeemachine.machine.CoffeeMachine;
 
@@ -11,8 +15,9 @@ public class Demo {
         coffeeMachine = new CoffeeMachine(5);
 
         coffeeMachine.brewBeverage(1, HOT_WATER);
-        coffeeMachine.brewBeverage(1, HOT_WATER);
-        coffeeMachine.brewBeverage(2, COFFEE);
-        coffeeMachine.brewBeverage(3, HOT_TEA);
+        coffeeMachine.brewBeverage(2, HOT_TEA);
+        coffeeMachine.brewBeverage(3, COFFEE);
+        coffeeMachine.brewBeverage(4, BLACK_TEA);
+        coffeeMachine.brewBeverage(5, GREEN_TEA);
     }
 }
